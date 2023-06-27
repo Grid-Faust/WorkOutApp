a166d944bf30724d0bc41834946babbbd1e3a423
Faust <mrtracker77@gmail.com>
2023-06-27 15:32:35 +0300

    setup Firebase

--- WorkOutAppApp.swift
+++ WorkOutAppApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct WorkOutAppApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             LandingView()
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    print("!!! PRINT: - setting Firebase")
+    FirebaseApp.configure()
+    return true
+  }
+}
+
